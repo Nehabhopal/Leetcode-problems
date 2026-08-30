58039e8ff212f91d31f50c80ffcda7e909d0c98e
Nehabhopal <nehabhopal83@gmail.com>
2026-08-30 13:06:01 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (58.32%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        total_sum=sum(nums)
+        left_sum=0
+        n=len(nums)
+
+        for i in range(n):
+            right_sum=total_sum-left_sum-nums[i]
+            if left_sum==right_sum:
+                return i
+            left_sum+=nums[i]
+
+        return -1        
